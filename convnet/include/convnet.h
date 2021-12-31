5f6eb7195273449afb8e60bed89ebf0f800d512e
vietnhatthai <thaivietnhat02@gmail.com>
2021-12-31 22:41:39 +0700

    ADD: ConvNetImpl

--- convnet/include/convnet.h
+++ convnet/include/convnet.h
@@ -1,1 +1,43 @@
 #pragma once
+#include <torch/torch.h>
+
+class ConvNetImpl : public torch::nn::Module {
+public:
+	explicit ConvNetImpl(int64_t num_classes);
+	torch::Tensor forward(torch::Tensor x);
+
+private:
+
+	torch::nn::Sequential layer1{
+		torch::nn::Conv2d(torch::nn::Conv2dOptions(3, 32, 5)),
+		torch::nn::BatchNorm2d(32),
+		torch::nn::ReLU(),
+		torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2))
+	};
+
+	torch::nn::Sequential layer2{
+		torch::nn::Conv2d(torch::nn::Conv2dOptions(32, 64, 5)),
+		torch::nn::BatchNorm2d(64),
+		torch::nn::ReLU(),
+		torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2))
+	};
+
+	torch::nn::Sequential layer3{
+		torch::nn::Conv2d(torch::nn::Conv2dOptions(64, 128, 5)),
+		torch::nn::BatchNorm2d(128),
+		torch::nn::ReLU(),
+		torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2))
+	};
+
+	torch::nn::Sequential layer4{
+		torch::nn::Conv2d(torch::nn::Conv2dOptions(128, 128, 5)),
+		torch::nn::BatchNorm2d(128),
+		torch::nn::ReLU(),
+		torch::nn::MaxPool2d(torch::nn::MaxPool2dOptions(2))
+	};
+
+	torch::nn::Linear fc1 = torch::nn::Linear(128 * 10 * 10, 50);
+	torch::nn::Linear fc2;
+};
+
+TORCH_MODULE(ConvNet);
--- convnet/src/convnet.cpp
+++ convnet/src/convnet.cpp
@@ -1,1 +1,32 @@
 #include "convnet.h"
+
+ConvNetImpl::ConvNetImpl(int64_t num_classes) :
+	fc2(50, num_classes)
+{
+	register_module("layer1", layer1);
+	register_module("layer2", layer2);
+	register_module("layer3", layer3);
+	register_module("layer4", layer4);
+	register_module("fc1", fc1);
+	register_module("fc2", fc2);
+}
+
+torch::Tensor ConvNetImpl::forward(torch::Tensor x) {
+	//std::cout << "Layer1" << std::endl;
+	x = layer1->forward(x);
+	//std::cout << "Layer2" << std::endl;
+	x = layer2->forward(x);
+	//std::cout << "Layer3" << std::endl;
+	x = layer3->forward(x);
+	//std::cout << "Layer4" << std::endl;
+	x = layer4->forward(x);
+	//std::cout << "Layer5" << std::endl;
+	x = x.view({ -1, 128 * 10 * 10 });
+	x = fc1->forward(x);
+	x = torch::relu(x);
+	//std::cout << "Layer6" << std::endl;
+	x = fc2->forward(x);
+	//x = torch::softmax(x, 1);
+
+	return x;
+}
