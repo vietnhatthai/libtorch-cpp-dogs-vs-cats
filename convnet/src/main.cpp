ae61baa71e49754ff2a9e08f202ff6122a67f058
vietnhatthai <thaivietnhat02@gmail.com>
2021-12-31 22:49:33 +0700

    ADD: Hyperparameters

--- convnet/src/main.cpp
+++ convnet/src/main.cpp
@@ -24,5 +24,17 @@ int main()
 	torch::Device device(cuda_available ? torch::kCUDA : torch::kCPU);
 	std::cout << (cuda_available ? "CUDA available. Training on GPU." : "Training on CPU.") << '\n';
 
+	std::string data_path = "D:\\Projects\\pytorch-test\\data\\dogcat\\train";  // data root
+	std::vector<std::string> name_classes = { "dog", "cat" };
+
+	// Hyperparameters
+	const int NUM_IMAGES = 25000;					// 12500 -cat, 12500 -dog
+	const int NUM_CLASSES = name_classes.size();	// 2
+	const int INPUT_SIZE = 224;						// 224 -w, 224 -h
+	const int BATCH_SIZE = 64;
+	const double LEARING_RATE = 0.001;
+	const size_t NUM_EPOCHS = 5;
+	const float TESTSET_SIZE = 0.2;					// 20 -val, 80 -train
+
 	return 0;
 }
