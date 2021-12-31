bab791577842fe0ff05c02cc355790918c1c4eb1
vietnhatthai <thaivietnhat02@gmail.com>
2021-12-31 22:39:51 +0700

    UPDATE: include libs

--- convnet/src/main.cpp
+++ convnet/src/main.cpp
@@ -1,5 +1,22 @@
 #include <torch/torch.h>
 
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
+#include <map>
+#include <algorithm>
+#include <random>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <tuple>
+#include <iostream>
+#include <iomanip>
+
+#include "convnet.h"
+
 int main()
 {
 
